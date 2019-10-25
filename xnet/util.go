ec3bf28c5772d53255987217cf31bb48707b37aa
闾明 <15925754@qq.com>
2019-10-25 19:11:34 +0800

    Create util.go
    增加获取外部ip的方法

---
+++ xnet/util.go
@@ -0,0 +1,16 @@
+package xnet
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func GetExternalIP() string {
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	content, _ := ioutil.ReadAll(resp.Body)
+	return string(content)
+}
